a717a89c24d16cbff001658478cd57347554c156
amaksimovic <amaksimovic84@gmail.com>
2019-03-17 22:44:45 +0100

    Deleted useless classes in body

--- header.php
+++ header.php
@@ -17,13 +17,7 @@
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
     </head>
 
-    <?php
-
-      $ana_classes = array( 'ana-class', 'my-class' );
-
-    ?>
-
-    <body <?php body_class( $ana_classes ); ?>>
+    <body>
 
       <div class="jumbotron jumbotron-fluid mb-2">
 
